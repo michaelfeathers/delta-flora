e7c36e621ca45fd5ea4a2ec96b1f002ffb0b2cfe
Michael Feathers <mfeathers@acm.org>
2015-01-03 15:07:12 -0500

    removed older files

--- writer_csv.rb
+++
@@ -1,6 +0,0 @@
-
-def write_events file_name, field_names, events
-  header = field_names.join( ',') + "\n"
-  rows = events.map { |e| field_names.map { |name| e.send(name).to_s  }.join(',') }.join($/)
-  File.open(file_name + ".csv", 'w') { |f| f.write(header + rows) }
-end
